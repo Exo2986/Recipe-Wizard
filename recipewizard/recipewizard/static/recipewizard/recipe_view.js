cbf316bca11d86fc21383bf94da55af3d9130307
David Hinkel <exomaster2986@gmail.com>
2022-04-16 15:37:13 -0400

    Implement serving size modification

--- recipewizard/recipewizard/static/recipewizard/recipe_view.js
+++ recipewizard/recipewizard/static/recipewizard/recipe_view.js
@@ -18,5 +18,34 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         })
         .catch(err => console.error(err));
-    };
+    };    
+
+    const servingCountInput = document.querySelector("#serving-count")
+    servingCountInput.dataset.originalServingCount = servingCountInput.value
+
+    const recipeAmounts = document.querySelectorAll(".ingredient-amount")
+
+    function updateRecipeAmounts() {
+        let ratio = servingCountInput.value / servingCountInput.dataset.originalServingCount
+        recipeAmounts.forEach((amount) => {
+            let updated = amount.dataset.originalAmount * ratio
+            
+            //round the new number to 2 decimal places
+            updated *= 100
+            updated = Math.round(updated)
+            updated /= 100
+
+            amount.innerHTML = updated
+        })
+    }
+
+    servingCountInput.addEventListener("input", () => {
+        servingCountInput.value.replaceAll(".","")
+
+        if (servingCountInput.value != "")
+            servingCountInput.value = Math.max(1, Math.min(100, servingCountInput.value)) //Clamp between 1 and 100
+
+        if (servingCountInput.value > 0)
+            updateRecipeAmounts()
+    })
 });
